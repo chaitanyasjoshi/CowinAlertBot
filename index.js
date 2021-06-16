b95cd75cc69f1e4114640fbf3bcbe355d898b252
Deadpool812 <chaitanya9joshi@gmail.com>
2021-06-16 09:48:51 +0530

    Fetch slots every 6s

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const fetch = require('node-fetch');
+const schedule = require('node-schedule');
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start(async (ctx) => {
   ctx.reply('Enter your pincode');
-  bot.hears(/^[1-9][0-9]{5}$/, async (ctx) => {
-    const sessions = await fetchSlots(ctx.message.text);
 
-    sessions.map((session) => {
-      ctx.reply(
-        `Pincode: ${session.pincode}, ${session.district_name}
+  bot.hears(/^[1-9][0-9]{5}$/, async (ctx) => {
+    const job = schedule.scheduleJob('*/6 * * * * *', async () => {
+      const sessions = await fetchSlots(ctx.message.text);
+
+      sessions.map((session) => {
+        ctx.reply(
+          `Pincode: ${session.pincode}, ${session.district_name}
 
 Age Group: ${session.min_age_limit}+
 
@@ -21,11 +25,17 @@ Date: ${session.date}
 Dose Available: ${session.available_capacity}
 
 Vaccine: ${session.vaccine}
-Fee: ₹ ${session.fee_type === 'Paid' ? session.fee : session.fee_type}`
-      );
+Fee: ${
+            session.fee_type === 'Paid' || session.fee === 0
+              ? '₹ ' + session.fee
+              : session.fee_type
+          }`
+        );
+      });
     });
   });
 });
+
 const fetchSlots = async (pincode) => {
   let today = new Date();
   const dd = today.getDate().toString().padStart(2, '0');
