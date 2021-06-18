1fa1af9332edf7025828078c1d1f3e82b12600f4
Deadpool812 <chaitanya9joshi@gmail.com>
2021-06-18 22:13:07 +0530

    add additional commands

--- index.js
+++ index.js
@@ -5,16 +5,34 @@ const schedule = require('node-schedule');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+let pincode;
+
 bot.start(async (ctx) => {
+  ctx.reply('How to get started guide');
+  // TODO: Add description of how to get started
+});
+
+bot.command('setpincode', (ctx) => {
   ctx.reply('Enter your pincode');
 
   bot.hears(/^[1-9][0-9]{5}$/, async (ctx) => {
-    const job = schedule.scheduleJob('*/6 * * * * *', async () => {
-      const sessions = await fetchSlots(ctx.message.text);
+    pincode = ctx.message.text;
+  });
+});
 
-      sessions.map((session) => {
-        ctx.reply(
-          `Pincode: ${session.pincode}, ${session.district_name}
+bot.command('notify', (ctx) => {
+  pincode
+    ? notify(ctx)
+    : ctx.reply('Please set pincode using /setpincode command');
+});
+
+const notify = async (ctx) => {
+  const job = schedule.scheduleJob('*/6 * * * * *', async () => {
+    const sessions = await fetchSlots();
+
+    sessions.map((session) => {
+      ctx.reply(
+        `Pincode: ${session.pincode}, ${session.district_name}
 
 Age Group: ${session.min_age_limit}+
 
@@ -28,17 +46,16 @@ Dose 2 Available Capacity: ${session.available_capacity_dose2}
 
 Vaccine: ${session.vaccine}
 Fee: ${
-            session.fee_type === 'Free' || session.fee === '0'
-              ? 'Free'
-              : '₹ ' + session.fee
-          }`
-        );
-      });
+          session.fee_type === 'Free' || session.fee === '0'
+            ? 'Free'
+            : '₹ ' + session.fee
+        }`
+      );
     });
   });
-});
+};
 
-const fetchSlots = async (pincode) => {
+const fetchSlots = async () => {
   let today = new Date();
   const dd = today.getDate().toString().padStart(2, '0');
   const mm = (today.getMonth() + 1).toString().padStart(2, '0');
